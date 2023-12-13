368567207e9ada865a0f5ac3544e7d46568bad2a
vigs950813 <renovatio_bluesunset@hotmail.com>
2023-12-12 20:44:06 -0600

    Se anexan funcionalidades básicas de conexión de nodos

--- js/nodes.js
+++ js/nodes.js
@@ -2,7 +2,7 @@
 const svg = d3.select('svg');
 const width = +svg.attr('width');
 const height = +svg.attr('height');
-const margin = 50; // Tamaño del margen
+const margin = 50; // Tamaño de margen (ees el área entre el borde del svg y los límites de los círculos)
 
 // Definir las coordenadas iniciales del primer círculo
 let circle1X = 100;
@@ -17,23 +17,31 @@ const circle1 = svg.append('circle')
   .attr('class', 'draggable-circle')
   .attr('cx', circle1X)
   .attr('cy', circle1Y)
-  .attr('r', 30);
+  .attr('r', 30)
+  .attr('stroke', 'black')
+  .attr('stroke-width', 2)
+  .attr('fill', '#BFBFBF');
 
 // Crear el segundo círculo en el SVG
 const circle2 = svg.append('circle')
   .attr('class', 'draggable-circle')
   .attr('cx', circle2X)
   .attr('cy', circle2Y)
-  .attr('r', 30);
+  .attr('r', 30)
+  .attr('stroke', 'black')
+  .attr('stroke-width', 2)
+  .attr('fill', '#BFBFBF');
 
 // Crear una línea para conectar los círculos
 const line = svg.append('line')
   .attr('stroke', 'black')
-  .attr('stroke-width', 2);
+  .attr('stroke-width', 4);
 
 // Funciones para mover el primer círculo dentro de los límites
 function dragstarted1(event) {
-  circle1.raise().attr('stroke', 'black'); // Levantar el círculo y cambiar el color del borde al ser arrastrado
+  circle1.raise().attr('stroke', 'blue'); // Levantar el círculo y cambiar el color del borde al ser arrastrado
+  circle1.raise().attr('fill', '#00CC3B');
+  circle1.raise().attr('opacity', 0.5);
 }
 
 function dragged1(event) {
@@ -49,12 +57,16 @@ function dragged1(event) {
 }
 
 function dragended1(event) {
-  circle1.attr('stroke', null); // Restaurar el color del borde al finalizar el arrastre
+  circle1.attr('stroke', 'black'); // Restaurar el color del borde al finalizar el arrastre
+  circle1.attr('fill', '#00922A'); // Restaurar el color del borde al finalizar el arrastre
+  circle1.attr('opacity', 1); // Restaurar el color del borde al finalizar el arrastre
 }
 
 // Funciones para mover el segundo círculo dentro de los límites
 function dragstarted2(event) {
-  circle2.raise().attr('stroke', 'black'); // Levantar el círculo y cambiar el color del borde al ser arrastrado
+  circle2.raise().attr('stroke', 'blue'); // Levantar el círculo y cambiar el color del borde al ser arrastrado
+  circle2.raise().attr('fill', '#CC0000');
+  circle2.raise().attr('opacity', 0.5);
 }
 
 function dragged2(event) {
@@ -70,16 +82,47 @@ function dragged2(event) {
 }
 
 function dragended2(event) {
-  circle2.attr('stroke', null); // Restaurar el color del borde al finalizar el arrastre
+  circle2.attr('stroke', 'black'); // Restaurar el color del borde al finalizar el arrastre
+  circle2.attr('fill', 'red'); // Restaurar el color del borde al finalizar el arrastre
+  circle2.attr('opacity', 1); // Restaurar el color del borde al finalizar el arrastre
 }
 
-// Actualizar las coordenadas de la línea para conectar los círculos
-function updateLine() {
+// Actualizar las coordenadas de la línea para conectar los círculos (esta funcion hace que el pequeño trozo de línea que se halla dentro del radio de los círculos se refleje en pantalla)
+/*function updateLine() {
   line.attr('x1', circle1X)
     .attr('y1', circle1Y)
     .attr('x2', circle2X)
     .attr('y2', circle2Y);
+}*/
+
+
+// Actualizar las coordenadas de la línea para conectar los círculos (esta funcion hace que el pequeño trozo de línea que se halla dentro del radio de los círculos desaparezca para que la conexión se vea con mejor presentación)
+function updateLine() {
+  const circle1Radius = +circle1.attr('r');
+  const circle2Radius = +circle2.attr('r');
+
+  // Verificar si la línea intersecta con el radio del círculo 1
+  const dist1 = Math.sqrt((circle2X - circle1X) ** 2 + (circle2Y - circle1Y) ** 2);
+  const lineLength = Math.sqrt((circle2X - circle1X) ** 2 + (circle2Y - circle1Y) ** 2);
+  const factor1 = circle1Radius / lineLength;
+
+  // Coordenadas ajustadas para la línea que se extiende más allá del radio del círculo 1
+  const newX1 = circle1X + (circle2X - circle1X) * factor1;
+  const newY1 = circle1Y + (circle2Y - circle1Y) * factor1;
+
+  // Verificar si la línea intersecta con el radio del círculo 2
+  const factor2 = circle2Radius / lineLength;
+
+  // Coordenadas ajustadas para la línea que se extiende más allá del radio del círculo 2
+  const newX2 = circle1X + (circle2X - circle1X) * (1 - factor2);
+  const newY2 = circle1Y + (circle2Y - circle1Y) * (1 - factor2);
+
+  line.attr('x1', newX1)
+    .attr('y1', newY1)
+    .attr('x2', newX2)
+    .attr('y2', newY2);
 }
+
 
 // Establecer la función de arrastre para el primer círculo utilizando D3
 circle1.call(d3.drag()
@@ -92,6 +135,8 @@ circle2.call(d3.drag()
   .on('start', dragstarted2)
   .on('drag', dragged2)
   .on('end', dragended2));
+
+
 
 // Actualizar la posición inicial de la línea al cargar la página
 updateLine();
